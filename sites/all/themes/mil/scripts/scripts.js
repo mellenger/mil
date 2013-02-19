1bba887e9c2bb59fcd9759f048182835a8569b25
Graham Holborn <graham@mellenger.com>
2013-02-19 14:39:46 -0800

    updates

---
+++ sites/all/themes/mil/scripts/scripts.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Attaches behaviors for the Contextual module.
+ */
+
+(function ($) {
+
+
+
+Drupal.behaviors.imagesizeSwap = {
+  attach: function (context) {
+
+    // $("article.node-article img").each(function(){
+    //   var path = $(this).attr('src');
+    //   path = path.split("files");
+    //   path = path[0] + "files/styles/large/public/" + path[1];
+    //   $(this).attr('src', path);
+    // });
+
+  }
+};
+
+
+})(jQuery);
