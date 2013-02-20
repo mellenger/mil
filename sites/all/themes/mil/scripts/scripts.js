bba6670face04745b5f8f623f2088631a41aa84a
Graham Holborn <graham@mellenger.com>
2013-02-20 09:44:01 -0800

    home slider

--- sites/all/themes/mil/scripts/scripts.js
+++ sites/all/themes/mil/scripts/scripts.js
@@ -20,5 +20,31 @@ Drupal.behaviors.imagesizeSwap = {
   }
 };
 
+Drupal.behaviors.teamcycle = {
+	attach: function (context){
+
+		var i=0;
+		$('#panels-ipe-paneid-5 .view-footer a').each(function(){
+			$(this).attr('data-slide-index', i);
+			i++;
+		});
+
+		$('#panels-ipe-paneid-5 ul').bxSlider({
+			pagerCustom: '#panels-ipe-paneid-5 .view-footer .view-content'
+		});
+	}
+};
+
+Drupal.behaviors.homepageselector = {
+	attach: function(context){
+
+		$('.region-two-66-33-bottom h2.pane-title').click(function(){
+			$('.region-two-66-33-bottom h2.pane-title').not(this).parent().find('.block-content').hide(500);
+			$(this).parent().find('.block-content').show(500);
+		});
+
+		$('.region-two-66-33-bottom h2.pane-title').eq(0).click();
+	}
+};
 
 })(jQuery);
