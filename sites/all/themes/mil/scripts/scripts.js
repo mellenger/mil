912e514c9b186e66ec5108c0107a1afdb3a854bd
Andrew Mellenger <andrew@mellenger.com>
2013-02-20 22:13:10 -0800

    show/hide arrows

--- sites/all/themes/mil/scripts/scripts.js
+++ sites/all/themes/mil/scripts/scripts.js
@@ -20,30 +20,38 @@
 //   }
 // };
 
-Drupal.behaviors.teamcycle = {
-	attach: function (context){
+// Drupal.behaviors.teamcycle = {
+// 	attach: function (context){
 
-		$('.view-team-members .view-header a').each(function(i){
-			$(this).attr('data-slide-index', i);
-		});
+// 		$('.view-team-members .view-header a').each(function(i){
+// 			$(this).attr('data-slide-index', i);
+// 		});
 
-		$('.view-team-members .view-content ul').bxSlider({
-			pagerCustom: '.view-team-members .view-header .view-content'
-		});
-	}
-};
+// 		$('.view-team-members .view-content ul').bxSlider({
+// 			pagerCustom: '.view-team-members .view-header .view-content'
+// 		});
+// 	}
+// };
 
 Drupal.behaviors.homepageselector = {
 	attach: function(context){
 
     var collapsePanels = "body.page-home .region-two-66-33-bottom h2.pane-title";
 
+    $(collapsePanels).prepend('<div class="arrow"></div>');
+
 		$(collapsePanels).click(function(){
-			$(collapsePanels).not(this).parent().find('.block-content').hide(500);
-			$(this).parent().find('.block-content').show(500);
+
+      if($(this).hasClass('open')){
+        $(this).removeClass('open').parent().find('.block-content').hide(500);
+      }else{
+        $(collapsePanels).not(this).removeClass('open').parent().find('.block-content').hide(500);
+        $(this).addClass('open').parent().find('.block-content').show(500);
+      }
+			
 		});
 
-		$(collapsePanels).eq(0).click();
+		$(collapsePanels).last().click();
 	}
 };
 
