3e0de9d812b6a05e232fdf17c4f7faa885a061fd
Graham Holborn <graham@mellenger.com>
2013-10-08 19:18:54 -0700

    slight changes

--- sites/all/themes/twitter_bootstrap/templates/page.tpl.php
+++ sites/all/themes/twitter_bootstrap/templates/page.tpl.php
@@ -1,4 +1,4 @@
-<header><?php print render($page['header']); ?></header>
+<header><div><?php print render($page['header']); ?></div></header>
 <main><?php print render($page['content']); ?></main>
 <nav><?php print render($page['content_bottom']); ?></nav>
-<footer><?php print render($page['footer']); ?></footer>
+<footer><div><?php print render($page['footer']); ?></div></footer>
