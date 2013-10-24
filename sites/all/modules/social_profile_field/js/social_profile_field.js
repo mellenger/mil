8e5159cc88977c6ebc37df85cf590cc8b5a8fe39
root <root@m3mm.(none)>
2013-10-24 01:07:31 -0500

    upgraded modules and core

--- sites/all/modules/social_profile_field/js/social_profile_field.js
+++ sites/all/modules/social_profile_field/js/social_profile_field.js
@@ -7,24 +7,24 @@
  * Show Social Network icon depending on URL.
  */
 (function ($) {
-  Drupal.behaviors.socialProfileField = {
-    attach: function(context) {
-      $(".edit-field-social-profile-url", context).live("change", function(event) {
-        if ($(this).val()) {
-          var url = $.url($(this).val());
-          var domain = url.attr('host').replace('.', '-').replace('.', '-');
+    Drupal.behaviors.socialProfileField = {
+        attach: function (context) {
+            $(".edit-field-social-profile-url", context).live("change", function (event) {
+                if ($(this).val()) {
+                    var url = $.url($(this).val());
+                    var domain = url.attr('host').replace('.', '-').replace('.', '-');
+                }
+                var $wrapper = $(this).parent('.form-type-textfield');
+                if (domain) {
+                    $wrapper.attr('rel', domain).addClass(domain);
+                } else {
+                    var domain = $wrapper.attr('rel');
+                    if (domain) {
+                        $wrapper.removeAttr('rel').removeClass(domain);
+                    }
+                }
+            });
+            $(".edit-field-social-profile-url", context).change();
         }
-        var $wrapper = $(this).parent('.form-type-textfield');
-        if (domain) {
-          $wrapper.attr('rel', domain).addClass(domain);
-        } else {
-          var domain = $wrapper.attr('rel');
-          if (domain) {
-            $wrapper.removeAttr('rel').removeClass(domain);
-          }
-        }
-      });
-      $(".edit-field-social-profile-url", context).change();
-    }
-  };
+    };
 })(jQuery);
